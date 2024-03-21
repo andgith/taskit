2240f52928ea62e26bbcb96f3126e802180100d5
Andrew Hirst <andrew.hirst@visavvi.com>
2024-03-21 15:21:25 +0000

    Remove unused views

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::redirect('/', '/tasks')->name('home');
+
+Route::redirect('/dashboard', '/tasks')->name('dashboard');
 
 Route::get('tasks', \App\Livewire\Tasks\ShowTasks::class)
     ->middleware(['auth'])
     ->name('tasks');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
